7ef8be4c0f2dea0e583fbc8dc732f1bcfe98ad70
vocql <piotrpolish19@gmail.com>
2026-09-17 16:11:47 -0500

    Update settings-ui.js

--- leonida/js/settings-ui.js
+++ leonida/js/settings-ui.js
@@ -154,11 +154,7 @@
   ];
 
   var GEAR_SVG = '<svg viewBox="0 0 24 24" class="ls-gear-svg">' +
-    '<defs><linearGradient id="lsGearGrad" x1="0%" y1="0%" x2="100%" y2="100%">' +
-    '<stop offset="0%"><animate attributeName="stop-color" values="#ff6fa0;#4dd8ff;#c94bff;#7fe8ff;#ff6fa0" dur="6s" repeatCount="indefinite"/></stop>' +
-    '<stop offset="100%"><animate attributeName="stop-color" values="#4dd8ff;#ff6fa0;#7fe8ff;#c94bff;#4dd8ff" dur="6s" repeatCount="indefinite"/></stop>' +
-    '</linearGradient></defs>' +
-    '<g fill="none" stroke="url(#lsGearGrad)" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round">' +
+    '<g fill="none" stroke="currentColor" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round">' +
     '<circle cx="12" cy="12" r="3"/>' +
     '<path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 1 1-2.83 2.83l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-4 0v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 1 1-2.83-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1 0-4h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 1 1 2.83-2.83l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 4 0v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 1 1 2.83 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 0 4h-.09a1.65 1.65 0 0 0-1.51 1z"/>' +
     '</g></svg>';
